8ad3d0ba457e1dbc979ff8d43b057d53942f3e9c
5730289021-NN <norawit.n@student.chula.ac.th>
2018-02-17 21:06:30 +0700

    sdd \sdasd

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var request = require('request');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s listening to %s', server.name, server.url);
+   console.log("sent-2")
 });
   
 // Create chat connector for communicating with the Bot Framework Service
@@ -28,6 +29,7 @@ server.post('/api/messages', connector.listen());
 * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
+console.log("sent-1")
 
 var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
@@ -38,29 +40,42 @@ var headers = {
     'Prediction-Key': '9ba907306c8740cea52aabd508df5c94',
     'Content-Type' : 'application/json' 
 };
+var body = {
+    'Url' : 'http://www.moronface.com/make-pictures/funny-pictures-2010-2/dddd1-34982.jpg'
+}
 
+console.log("sent-31")
 
 // Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
+console.log("sent-11")
 
 bot.dialog('/', function (session) {
     var msg = session.message;
+    console.log("sent0")
     if (msg.attachments && msg.attachments.length > 0) {
      // Echo back attachment
+     console.log("sent1")
         request.post(
             {
                 url : url,
-                headers: headers
+                headers : headers,
+                body : body
             },
             function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     session.send({text: body});
+                    console.log("sent2")
                 }
             }
         );
+        console.log("sent3")
     } else {
+        console.log("sent4")
         // Echo back users text
         session.send("Skin said: %s", session.message.text);
+        console.log("sent5")
     }
+    console.log("sent6")
 });

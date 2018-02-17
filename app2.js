a06224b163c8b080da992f198adb0b11152aa8a8
5730289021-NN <norawit.n@student.chula.ac.th>
2018-02-18 02:46:27 +0700

    fads

--- app2.js
+++ app2.js
@@ -5,6 +5,8 @@ A simple echo bot for the Microsoft Bot Framework.
 var restify = require('restify');
 var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
+var request = require('request');
+var querystring = require('querystring');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -23,14 +25,28 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages2', connector.listen());
 
 
+
 // Create your bot with a function to receive messages from the user
 // Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector, function (session) {
     var msg = session.message;
     if (msg.attachments && msg.attachments.length > 0) {
      // Echo back attachment
-     var attachment = msg.attachments[0];
-        session.send({
+    var attachment = msg.attachments[0];
+    var form = { Url: attachment.contentUrl };
+    var formData = querystring.stringify(form);
+    request({
+        header: {
+            'Prediction-Key' : '9ba907306c8740cea52aabd508df5c94',
+            'Content-Type' : 'application/json'
+        },
+        uri: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/1ad8ba80-bd73-4e09-b185-260423589f69/url',
+        body: formData,
+        method: 'POST'
+    }, function (err, res, body){
+        console.log(body)
+    });
+        /*session.send({
             text: "You sent:",
             attachments: [
                 {
@@ -39,7 +55,8 @@ var bot = new builder.UniversalBot(connector, function (session) {
                     name: attachment.name
                 }
             ]
-        });
+        });*/
+
     } else {
         // Echo back users text
         session.send("You said: %s", session.message.text);

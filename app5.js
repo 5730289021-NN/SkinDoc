dc6dd5fc096895ff5162b340ca58554d7602d2e1
5730289021-NN <norawit.n@student.chula.ac.th>
2018-02-18 04:40:32 +0700

    asd

--- app5.js
+++ app5.js
@@ -1,42 +1,43 @@
 /*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
+A simple echo bot for the Microsoft Bot Framework.
 -----------------------------------------------------------------------------*/
 
 var restify = require('restify');
 var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
-var needle = require('needle');
+var fetch = require('node-fetch');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-  
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-// Listen for messages from users 
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({appId: process.env.MicrosoftAppId, appPassword: process.env.MicrosoftAppPassword, openIdMetadata: process.env.BotOpenIdMetadata});
+
+// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot.
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
 
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({
+    gzipData: false
+}, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector, function (session) {
-    var msg = session.message;
-    var options = {
-        headers: { 'Prediction-Key': '9ba907306c8740cea52aabd508df5c94',
-                   'Content-Type': 'application/json'}
-    }
-    var data = {
-        Url: 'https://cdn.pixabay.com/photo/2013/04/06/11/50/image-editing-101040_960_720.jpg'
-    }
-    if (msg.attachments && msg.attachments.length > 0) {
+var bot = new builder.UniversalBot(connector);
+bot.set('storage', tableStorage);
+
+bot.dialog('/', function (session) {
+    //console.log(session.message.attachments[0].contentUrl,'contentUrl');
+    if (session.message.attachments.length > 0) {
         api('https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/' +
                 '1ad8ba80-bd73-4e09-b185-260423589f69/url',
         session.message.attachments[0].contentUrl).then((output) => {
@@ -69,4 +70,3 @@ function api(url, picUrl) {
             return data;
         });
 }
-
